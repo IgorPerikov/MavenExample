6113ac6dcb4a51a26808815c1a59d043cb4b7af5
Igor <perikov.igor@gmail.com>
2013-07-14 00:05:09 +0700

    deleted println's

--- MavenExample/src/main/java/ru/tsu/csd/Time.java
+++ MavenExample/src/main/java/ru/tsu/csd/Time.java
@@ -6,8 +6,6 @@ public class Time
 {
 	public static void main(String []args)
 	{
-		System.out.println("123123");
-		System.out.println("123123");
 		String dt = new java.text.SimpleDateFormat(("hh:mm aaa")).format(java.util.Calendar.getInstance().getTime());
 		JOptionPane.showMessageDialog(null, "Current time: " + dt, "Time", JOptionPane.INFORMATION_MESSAGE);
 	}
