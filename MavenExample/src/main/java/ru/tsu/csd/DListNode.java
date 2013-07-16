3f0bec678cc7eb52d22dab26aeeccbf464ba818e
Igor <perikov.igor@gmail.com>
2013-07-16 14:58:23 +0700

    added setters for DListNode

--- MavenExample/src/main/java/ru/tsu/csd/DListNode.java
+++ MavenExample/src/main/java/ru/tsu/csd/DListNode.java
@@ -11,13 +11,28 @@ public class DListNode
 		return next;
 	}
 	
-	public DListNode getprev()
+	public void setNext(DListNode dln)
+	{
+		next = dln;
+	}
+	
+	public DListNode getPrev()
 	{
 		return prev;
+	}
+	
+	public void setPrev(DListNode dln)
+	{
+		prev = dln;
 	}
 	
 	public int getValue()
 	{
 		return value;
+	}
+	
+	public void setValue(int a)
+	{
+		value = a;
 	}
 }
