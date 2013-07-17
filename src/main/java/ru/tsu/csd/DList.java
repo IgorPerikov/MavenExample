6725cae7ecc44f54e7d50496244c0c87a4ffd3db
Igor <perikov.igor@gmail.com>
2013-07-17 23:07:48 +0700

    start coding adding in list

--- src/main/java/ru/tsu/csd/DList.java
+++ src/main/java/ru/tsu/csd/DList.java
@@ -2,5 +2,26 @@ package ru.tsu.csd;
 
 public class DList 
 {
+	DListNode FirstNode;
 	
+	public DList(int value) 
+	{
+		FirstNode = new DListNode(value);
+		FirstNode.setNext(FirstNode);
+		//FirstNode.setPrev(FirstNode);
+	}
+	
+	public void Add(DListNode dln) 
+	{
+		DListNode NextNode = FirstNode.getNext();
+		while(NextNode.getNext() != FirstNode)
+		{
+			NextNode = NextNode.getNext();
+		}
+		NextNode = dln;
+	}
+	//add at the end
+	//size
+	//create
+	//test
 }
