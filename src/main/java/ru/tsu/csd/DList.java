8ec874ac14222c64215e38e0360c6a36f6c7c305
Igor <perikov.igor@gmail.com>
2013-07-19 00:55:41 +0700

    delete cycle

--- src/main/java/ru/tsu/csd/DList.java
+++ src/main/java/ru/tsu/csd/DList.java
@@ -2,40 +2,39 @@ package ru.tsu.csd;
 
 public class DList 
 {
-	DListNode FirstNode;
+	public DListNode firstNode;
 	
 	public DList(int value) 
 	{
-		FirstNode = new DListNode(value);
-		FirstNode.setNext(FirstNode);
-		FirstNode.setPrev(FirstNode);
+		firstNode = new DListNode(value);
+		firstNode.setNext(firstNode);
+		firstNode.setPrev(null);
 	}
 	
 	public void add(int value) 
 	{
-		DListNode NextNode = FirstNode.getNext();
-		while(NextNode.getNext() != FirstNode)
+		DListNode NextNode = firstNode.getNext();
+		while(NextNode.getNext() != null)
 		{
 			NextNode = NextNode.getNext();
 		}
 		DListNode dln = new DListNode(value);
 		NextNode.setNext(dln);
 		dln.setPrev(NextNode);
-		dln.setNext(FirstNode);
-		FirstNode.setPrev(dln);
+		dln.setNext(firstNode);
+		firstNode.setPrev(dln);
 	}
 	
 	public int size()
 	{
-		DListNode NextNode = FirstNode.getNext();
+		DListNode NextNode = firstNode.getNext();
 		int i = 1;
-		while(NextNode.getNext() != FirstNode)
+		while(NextNode.getNext() != null)
 		{
 			NextNode = NextNode.getNext();
 			i++;
 		}
-		if (FirstNode.getNext() != FirstNode) i++;
+		if (firstNode.getNext() != firstNode) i++;
 		return i;
 	}
-	//test
 }
