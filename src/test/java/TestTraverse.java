38e6c0161805c26071cc90f6fe87f908f817631d
Igor <perikov.igor@gmail.com>
2013-07-19 00:54:52 +0700

    add new test

---
+++ src/test/java/TestTraverse.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import ru.tsu.csd.DList;
+import ru.tsu.csd.DListNode;
+
+public class TestTraverse 
+{
+ 
+    @Test
+    public void testTraversing() 
+    {
+        DList list = new DList(25);
+        list.add(26);
+        list.add(27);
+        while (list.firstNode.getNext() != null) 
+        {
+            DListNode next = list.firstNode.getNext();
+            System.out.println(next.getValue());
+        }
+ 
+    }
+}
