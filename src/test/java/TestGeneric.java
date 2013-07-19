2fa30cfe626309c3eaa348e557d07be7d1dc8cef
Igor <perikov.igor@gmail.com>
2013-07-19 21:44:58 +0700

    generic test

---
+++ src/test/java/TestGeneric.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import ru.tsu.csd.DList;
+
+
+public class TestGeneric 
+{
+	@Test
+	public void test() 
+	{
+		Double d = new Double(123.32);
+		DList<Double> list = new DList<>(d);
+		list.add(32.32);
+		list.add(54.21);
+		Double d1 = list.getFirstNode().getNext().getValue();
+		System.out.println(d1);
+	}
+
+}
